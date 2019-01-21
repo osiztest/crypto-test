ce6bf772947c040f085f9977b72b53d44e5077cb
osiztest <46840261+osiztest@users.noreply.github.com>
2019-01-21 13:30:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -33,31 +33,29 @@ db.once('open', function callback () {
 })
 
 app.post('/cryptovalue',function(req, res) {
+  var val1=req.body.queryResult.parameters['cryptocoin'];
+  var url="https://api.cryptonator.com/api/ticker/";
+const rp = require('request-promise');
+const requestOptions = {
+  method: 'GET',
+  uri:url+'/'+val1+'/usd';
+ };
 
-console.log("sdgs")
+rp(requestOptions).then(response => {
+  var data=JSON.parse(response)
+  var value={
+    "fullfllmentText":"the current value is"+data.ticker.price,
+    "fullfllmentMessages":[{"text":{"text":"the current value is"+data.ticker.price}}],
+    "source":""
+  }
+return res.json(value)
+}).catch((err) => {
+  console.log('API call error:', err.message);
+});
 
 
 })
 
-// const rp = require('request-promise');
-// const requestOptions = {
-//   method: 'GET',
-//   uri: 'https://api.cryptonator.com/api/ticker/usd-btc',
- 
-// };
-
-// rp(requestOptions).then(response => {
-//   var data=JSON.parse(response)
-//   var value={
-//     "firstcoin":data.ticker.base,
-//     "secondcoin":data.ticker.target,
-//     "value":data.ticker.price
-//   }
-//     console.log(value)
-
-// }).catch((err) => {
-//   console.log('API call error:', err.message);
-// });
 
 
 
