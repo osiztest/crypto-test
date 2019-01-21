7662d24c848371c324c8f71913c0b2eb371166dd
osiztest <46840261+osiztest@users.noreply.github.com>
2019-01-21 15:20:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,48 +9,43 @@ const hostname = '0.0.0.0';
 const port = process.env.PORT || 3000;
 
 //Port and database connectivity
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
 
 
 
-app.post('/cryptovalue',function(req, res) {
-  var val1="btc"
-  var url="https://api.cryptonator.com/api/ticker/";
-  var uur=url+val1+'-usd';
+// app.post('/cryptovalue',function(req, res) {
+//   var val1="btc"
+//   var url="https://api.cryptonator.com/api/ticker/";
+//   var uur=url+val1+'-usd';
 
-const rp = require('request-promise');
-const requestOptions = {
-  method: 'GET',
-  uri:uur
- };
+// const rp = require('request-promise');
+// const requestOptions = {
+//   method: 'GET',
+//   uri:uur
+//  };
 
-rp(requestOptions).then(response => {
-  var data=JSON.parse(response)
-  var value={
-    "fullfllmentText":"the current value is "+data.ticker.price
-    }
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end(value);
-}).catch((err) => {
-  console.log('API call error:', err.message);
-});
-})
+// rp(requestOptions).then(response => {
+//   var data=JSON.parse(response)
+//   var value={
+//     "fullfllmentText":"the current value is "+data.ticker.price
+//     }
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end(value);
+// }).catch((err) => {
+//   console.log('API call error:', err.message);
+// });
+// })
  
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
 
-app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'POST,GET');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-});
 
 
